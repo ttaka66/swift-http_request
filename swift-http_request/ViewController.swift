9ce5b9a9ec4dc4dbbfe12ae01834bef45b9aea03
ttaka66 <d.m_ro.nl.e@icloud.com>
2017-04-23 09:51:13 +0900

    Initial Commit

---
+++ swift-http_request/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-http_request
+//
+//  Created by 土屋貴大 on 2017/04/23.
+//  Copyright © 2017年 ttaka66. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
